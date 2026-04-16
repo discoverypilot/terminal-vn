9d875ef7104e42390d417bc98aafab26736464d2
Hal <shinycelebi@protonmail.com>
2026-04-16 18:41:25 -0400

    Get basic screen drawing class online

--- engine.cpp
+++ engine.cpp
@@ -1,3 +1,41 @@
 #include "engine.h"
 
+void Canvas::assignBorders() {
+  for (int x = 0; x < SCREEN_WIDTH; ++x) {
+    canvas[x][0] = "═";
+    canvas[x][SCREEN_HEIGHT - 1] = "═";
+    canvas[x][SCREEN_HEIGHT - 1 - TEXTBOX_HEIGHT] = "═";
+  }
 
+  for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+    canvas[0][y] = "║";
+    canvas[SCREEN_WIDTH-1][y] = "║";
+  }
+
+  canvas[0][0] = "╔";
+  canvas[0][SCREEN_HEIGHT-1] = "╚";
+  canvas[SCREEN_WIDTH - 1][0] = "╗";
+  canvas[SCREEN_WIDTH-1][SCREEN_HEIGHT-1] = "╝";
+  canvas[0][SCREEN_HEIGHT - 1 - TEXTBOX_HEIGHT] = "╠";
+  canvas[SCREEN_WIDTH - 1][SCREEN_HEIGHT - 1 - TEXTBOX_HEIGHT] = "╣";
+}
+
+Canvas::Canvas() {
+  for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+    for (int x = 0; x < SCREEN_WIDTH; ++x) {
+      canvas[x][y] = " ";
+    }
+  }
+  assignBorders();
+}
+
+void Canvas::draw() {
+  clear();
+  for (int y = 0; y < SCREEN_HEIGHT; ++y) {
+    for (int x = 0; x < SCREEN_WIDTH; ++x) {
+      std::cout << canvas[x][y];
+    }
+    std::cout << "\n";
+  }
+  std::cout << std::flush;
+}
--- main.cpp
+++ main.cpp
@@ -1,31 +1,9 @@
-//#include "engine.h"
-#include <iostream>
-#include <ostream>
-
-#define SCREEN_WIDTH 96 // should be small enough to render w/o wrapping
-#define SCREEN_HEIGHT ((((SCREEN_WIDTH / 2) / 4) * 3)) // 4:3 image ratio accounting for ASCII character dimensions
+#include "engine.h"
 
 int main() {
-  std::cout << "\033[H\033[2J" << std::flush;
+  Canvas canvas;
 
-  std::cout << "\033[30;42m" << " hello world";
-  for (int i = 12; i < SCREEN_WIDTH; ++i) {
-    std::cout << " ";
-  }
-  std::cout << "\033[0m";
-  std::cout << "\n";
-
-
-  std::cout << "\033[40m";
-  for (int y = 1; y <= SCREEN_HEIGHT - 1; ++y) {
-    for (int x = 1; x <= SCREEN_WIDTH; ++x) {
-      
-      std::cout << " ";
-    }
-    std::cout << "\n";
-  }
-  std::cout << "\033[0m" << std::flush;
-
+  canvas.draw();
 
   return 0;
 }
