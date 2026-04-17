308479d7ba07afc5921ea2cab3b8a82cc0b93b53
Hal <shinycelebi@protonmail.com>
2026-04-17 10:10:32 -0400

    Remove scope creep (color is unneccesary)

--- engine.h
+++ engine.h
@@ -13,38 +13,6 @@ struct Screen {
   static const int fullscreen_height = (height - 2);
 };
 
-struct Color {
-  static const int black = 30;
-  static const int red = 31;
-  static const int green = 32;
-  static const int yellow = 33;
-  static const int blue = 34;
-  static const int magenta = 35;
-  static const int cyan = 36;
-  static const int white = 37;
-};
-
-struct BackgroundColor {
-  static const int black = 40;
-  static const int red = 41;
-  static const int green = 42;
-  static const int yellow = 43;
-  static const int blue = 44;
-  static const int magenta = 45;
-  static const int cyan = 46;
-  static const int white = 47;
-};
-
-struct TextEffect {
-  static const int reset = 0;
-  static const int bold = 1;
-  static const int underline = 4;
-  static const int inverse = 7;
-  static const int bold_off = 21;
-  static const int underline_off = 24;
-  static const int inverse_off = 27;
-};
-
 class Canvas {
 private:
   std::string canvas[Screen::width][Screen::height];
